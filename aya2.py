e70185d94b5030626b95c19562203ff0f0e33b17
ayasayedmohamed2006-sudo <ayasayedmohamed2006@gmail.com>
2025-09-03 19:16:09 +0300

    Add files via upload

---
+++ aya2.py
@@ -0,0 +1,9 @@
+temp=int(input('enter the current temperature in celsius:'))
+if temp >=30 :
+   print("It's a hot day .Stay hydrated!")
+elif temp <=29 and temp >=20 :
+    print("It's a warm day. Enjoy the weather!")  
+elif temp <=19 and temp >=10 :
+    print("It's a cool day.Wear a jacket!") 
+elif temp  <10 :
+    print("It's a cold day. Stay warm!")
